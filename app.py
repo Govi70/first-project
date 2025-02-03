cc8c62dc94ae3491e3e22a0bface62d2d7d2f520
Govi70 <govintharajsg@gmail.com>
2025-02-03 10:43:10 +0000

    add html and css
    designing

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 print("hello world")
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello World , Hoe are you Govi..😊"
+  return render_template("home.html")
 
 print("Finished")
 
 if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(debug=True)
 
 
 
